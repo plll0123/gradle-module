42189882915bb4b48d34f975e7a03880a54fa523
plll0123 <jsh951227@gmail.com>
2023-06-26 23:16:26 +0900

    불필요한 코드 제거

--- multi-module-sample/multi-module-common/src/test/java/org/example/MemberRepositoryTest.java
+++ multi-module-sample/multi-module-common/src/test/java/org/example/MemberRepositoryTest.java
@@ -2,16 +2,11 @@ package org.example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import javax.transaction.Transactional;
 
 
 @DataJpaTest
-//@ExtendWith(SpringExtension.class)
 class MemberRepositoryTest {
 
     @Autowired
