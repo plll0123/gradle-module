4a4adb7a75dedb6cbc4e589060e90422442a9268
plll0123 <jsh951227@gmail.com>
2023-06-26 21:54:06 +0900

    member 엔티티 설정

--- multi-module-sample/multi-module-common/src/main/java/org/example/Member.java
+++ multi-module-sample/multi-module-common/src/main/java/org/example/Member.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    public Member(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 }
