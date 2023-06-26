cca0a7bebafff8b5042d0240908020042ee65249
plll0123 <jsh951227@gmail.com>
2023-06-26 21:55:03 +0900

    member repository 작성

--- multi-module-sample/multi-module-common/src/main/java/org/example/MemberRepository.java
+++ multi-module-sample/multi-module-common/src/main/java/org/example/MemberRepository.java
@@ -1,4 +1,6 @@
 package org.example;
 
-public class MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
